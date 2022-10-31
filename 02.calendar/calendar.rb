37dda20ceee897c259c5ffd2e85f1f30df198121
minato <km.minato@icloud.com>
2022-10-31 14:50:41 +0900

    変数定義位置の訂正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,14 +1,10 @@
 require "date"
 require "optparse"
 
-#header
+#title
 params = ARGV.getopts('y:', 'm:')
 year = params["y"]&.to_i || Date.today.year
 month = params["m"]&.to_i || Date.today.month
-first_day_of_week = Date.new(year, month, 1).wday
-end_month = Date.new(year, month, -1).day
-
-#title
 current_day = "#{month}月 #{year}"
 puts current_day.center(20)
 
@@ -17,7 +13,9 @@ weeks = %w(日 月 火 水 木 金 土)
 puts weeks.join(" ")
 
 #day
+first_day_of_week = Date.new(year, month, 1).wday
 blank_date = "   " * first_day_of_week
+end_month = Date.new(year, month, -1).day
 print blank_date
 (1..end_month).each do |date|
   print date.to_s.rjust(2) + " "
